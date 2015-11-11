6f264ee11892b8242227f4bb8685159ac0e4ba2e
20141105043 <670140530@qq.com>
2015-11-11 19:12:02 +0800

    指针
    建立一个整形指针变量p，整形变量a，然后让p存a的地址

---
+++ main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  work test
+//
+//  Created by 20141105043dj on 15/11/11.
+//  Copyright (c) 2015年 20141105043dj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+                int *p;
+                int a;
+                a=100;
+                p=&a;
+                printf("a=%d &a=%x p=%x &p=%x *p=%d",a,&a,p,&p,*p);
+                return 0;
+    }
+    return 0;
+}
